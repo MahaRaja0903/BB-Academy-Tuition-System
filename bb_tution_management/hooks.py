08f900fbde1adff2f8a9d434e045b460583d1630
Maharaja <vijaymaharajan38@gmail.com>
2026-08-12 22:49:56 +0530

    Fix:Hiddend the Helpp Button

--- bb_tution_management/hooks.py
+++ bb_tution_management/hooks.py
@@ -25,7 +25,7 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/bb_tution_management/css/bb_tution_management.css"
+app_include_css = "/assets/bb_tution_management/css/bb_tution_management.css"
 app_include_js = "/assets/bb_tution_management/js/bb_tution_management.js"
 
 # include js, css files in header of web template
